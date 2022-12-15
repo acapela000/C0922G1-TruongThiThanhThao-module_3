d11785d13628caadc26df1a49f3d9932fe6d5540
acapela000 <106527693+acapela000@users.noreply.github.com>
2022-12-15 15:30:25 +0700

    Update ss1_Relational Database_Exercise1_CreateTable.sql

--- ss1_Relational Database_Exercise1_CreateTable.sql
+++ ss1_Relational Database_Exercise1_CreateTable.sql
@@ -1,14 +1,14 @@
 /*Bài tập 1: tạo bảng dữ liệu mới*/
-CREATE DATABASE student_management;
+create database student_management;
 /*tạo bảng có tên là class*/
-CREATE TABLE class (
-id int,
-name varchar(255)
+create table class (
+  id int,
+  name varchar(255)
 );
 /*tạo bảng có tên là teacher*/
-CREATE TABLE teacher (
-id int,
-name varchar(255),
-age int,
-country varchar(255)
+create table teacher (
+  id int,
+  name varchar(255),
+  age int,
+  country varchar(255)
 );
