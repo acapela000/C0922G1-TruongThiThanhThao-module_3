64c2a9d8f517a4c8de47c7eee2aeac544d1ae251
thao <mrtrados@gmail.com>
2022-12-19 21:47:24 +0700

    ss4_

---
+++ ss4_/ss4_common_function.sql
@@ -0,0 +1,16 @@
+drop database if exists ss4_common_function;
+create database ss4_common_function;
+use ss3_student_management;
+
+-- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
+select max(credit) as 'max credit' from `subject`;
+
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất.
+select m.mark as 'max mark', s.* from mark m
+join `subject` s where m.sub_id = s.sub_id and m.mark = (select max(mark) from mark);
+-- select max(mark) from mark;
+
+-- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
+select *, avg(m.mark) as avg_mark from student s
+join mark m where m.student_id = s.student_id
+group by s.student_id;
