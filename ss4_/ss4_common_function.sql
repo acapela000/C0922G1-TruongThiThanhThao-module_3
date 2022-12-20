f05ee8f67d320ebc31aaa02194f2fd7a280a2895
thao <mrtrados@gmail.com>
2022-12-20 15:06:21 +0700

    ss4_FixFeedback

--- ss4_/ss4_common_function.sql
+++ ss4_/ss4_common_function.sql
@@ -13,4 +13,5 @@ join `subject` s where m.sub_id = s.sub_id and m.mark = (select max(mark) from m
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
 select *, avg(m.mark) as avg_mark from student s
 join mark m where m.student_id = s.student_id
-group by s.student_id;
+group by s.student_id
+order by m.mark desc;
