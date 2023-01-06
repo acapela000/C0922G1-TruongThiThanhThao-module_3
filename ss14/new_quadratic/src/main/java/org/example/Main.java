202435a8aef726716d8b90513dce6dba5c83bb3c
thao <mrtrados@gmail.com>
2023-01-06 17:50:45 +0700

    ss14_exercise

---
+++ ss14/new_quadratic/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import ss14_quadratic_equation.QuadraticEquation;
+
+public class Main {
+    public static void main(String[] args) {
+        QuadraticEquation.quadratic(8,14,11);
+        System.out.println("Hello world!");
+    }
+}
